c004eaa587c2ec019f60f87733d68f8fe47389f1
Tong Guo <Tong-G@outlook.com>
2014-11-01 10:20:18 +0800

    Initial Commit

---
+++ GTShortcutLab/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GTShortcutLab
+//
+//  Created by Tong G. on 11/1/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
