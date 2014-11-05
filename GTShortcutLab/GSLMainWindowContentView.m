8b109a56717a167786ef29af7f01c43d8b078841
Tong Guo <Tong-G@outlook.com>
2014-11-06 01:02:16 +0800

    UPDATED MASShortcut submodule. For details, see here: https://github.com/TongG/MASShortcut

--- GTShortcutLab/GSLMainWindowContentView.m
+++ GTShortcutLab/GSLMainWindowContentView.m
@@ -57,7 +57,8 @@
 
             MASShortcutMonitor* monitor = [ MASShortcut addGlobalHotkeyMonitorWithShortcut: [ _Sender shortcutValue ]
                                                                                    handler: ^{ NSLog( @"Fuck!" ); } ];
-            self.monitor = [ monitor copy ];
+            if ( monitor )
+                self.monitor = [ monitor copy ];
             } ];
     }
 
@@ -69,52 +70,6 @@
 - ( BOOL ) acceptsFirstResponder
     {
     return YES;
-    }
-
-typedef void ( ^GSLPrintBlock )( void );
-- ( void ) handleBlocks: ( GSLPrintBlock )_Block
-    {
-    GSLPrintBlock anotherPrintFuckBlock = [ _Block copy ];
-    GSLPrintBlock fuckingBlock = [ anotherPrintFuckBlock copy ];
-
-    NSLog( @"%@", anotherPrintFuckBlock );
-    NSLog( @"%@", fuckingBlock );
-    }
-
-- ( void ) drawRect: ( NSRect )_DirtyRect
-    {
-    CGRect lhsRect = NSZeroRect;
-    CGRect rhsRect = NSZeroRect;
-
-    CGRectDivide( self.bounds, &lhsRect, &rhsRect, 20, CGRectMinXEdge );
-
-//    NSFrameRectWithWidth( lhsRect, 3.f );
-//    NSFrameRectWithWidth( rhsRect, 3.f );
-
-//    NSRect oneRect = NSMakeRect( 20, 20, 50, 50 );
-//    NSRect offsettedRect = NSRectFromCGRect( CGRectOffset( NSRectToCGRect( oneRect ), 10, 10 ) );
-//
-//    NSFrameRectWithWidth( oneRect, 3.f );
-//    NSFrameRectWithWidth( offsettedRect, 3.f );
-
-    [ super drawRect: _DirtyRect ];
-    }
-
-- ( void ) keyDown: ( NSEvent* )_Event
-    {
-#if 0
-    MASShortcut* shortcut = [ MASShortcut shortcutWithEvent: _Event ];
-    NSLog( @"%@", shortcut.keyCodeString );
-    NSLog( @"%@", shortcut.keyCodeStringForKeyEquivalent );
-    NSLog( @"%@", shortcut.modifierFlagsString );
-    NSLog( @"Complete: %@", shortcut.description );
-    printf( "\n\n" );
-
-    NSLog( @"%@", [ NSApp mainMenu ].itemArray );
-
-    NSLog( @"%@", MASShortcutChar( 0xF710 ) );
-#endif
-    [ super keyDown: _Event ];
     }
 
 #pragma mark IBActions
